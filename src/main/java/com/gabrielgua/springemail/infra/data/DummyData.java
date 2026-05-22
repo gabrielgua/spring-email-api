00c6eec7455e39d0535e17b9902978cff61c9faa
gabrielgua <gabrieljmym13@gmail.com>
2026-05-22 15:35:22 -0300

    chore: dummy data now only runs in the dev env

--- src/main/java/com/gabrielgua/springemail/infra/data/DummyData.java
+++ src/main/java/com/gabrielgua/springemail/infra/data/DummyData.java
@@ -9,12 +9,14 @@ import com.gabrielgua.springemail.domain.service.ProjectService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import java.time.Instant;
 import java.time.OffsetDateTime;
 import java.util.List;
 
 @Configuration
+@Profile("dev")
 public class DummyData {
 
     @Bean
